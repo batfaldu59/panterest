d14a29991b1df0ca7dd28fd61cdc1601e037e3b0
batfaldu59 <baptistefalbierski7@gmail.com>
2020-11-09 12:09:12 +0100

    Create pluralize : twig function, show pins

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,9 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(PinRepository $pinRepository): Response
     {
-        return $this->render('pins/index.html.twig');
+        $pins = $pinRepository->findAll();
+        return $this->render('pins/index.html.twig', compact('pins'));
     }
 }
