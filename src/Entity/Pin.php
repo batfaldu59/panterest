f19b7a27254a3b23dd99f59082c48104e2b3f5eb
batfaldu59 <baptistefalbierski7@gmail.com>
2020-11-12 11:39:30 +0100

    customize website with bootstrap

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -45,7 +45,7 @@ class Pin
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
